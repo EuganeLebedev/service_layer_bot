825c722a258bbe8a810f07d6d4beea9dd4ccdaa6
Eugane Lebedev <adm1n.lebedev@ya.ru>
2021-08-28 11:54:58 +0300

    Вынесены данные для подключения в отдельный файл Убран мусор

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ import json
 import telebot
 from telebot import types
 import requests
-
-bot_token = ""
-channel = '192151684'
+import settings
 
 
-bot = telebot.TeleBot(bot_token)
+
+bot = telebot.TeleBot(settings.bot_token)
 
 name = ''
 surname = ''
@@ -17,21 +16,18 @@ age = 0
 
 @bot.message_handler(content_types=['text'])
 def start(message):
-    if message.text == '/reg':
-        bot.send_message(message.from_user.id, "Как тебя зовут?")
-        # bot.register_next_step_handler(message, get_name)  # следующий шаг – функция get_name
-    elif message.text.lower() == 'tmp':
+    if message.text.lower() == 'документы':
         get_all_approval_requests(message)
     else:
-        bot.send_message(message.from_user.id, 'Напиши /reg')
+        bot.send_message(message.from_user.id, "Для вывода смиска докумендов на утверждение напиши Документы")
 
 
 
 def login_to_sld():
-    url = "https://192.168.221.171:50000/b1s/v1/Login"
+    url = settings.sld_url + "/Login"
 
     request_body = {
-        "CompanyDB": "",
+        "CompanyDB": "STRATNANOTECH_17_12_2020",
         "Password": "1111",
         "UserName": "rusak"
     }
@@ -44,27 +40,40 @@ def get_all_approval_requests(message):
 
     login_request = login_to_sld()
 
-    url = "https://192.168.221.171:50000/b1s/v1/ApprovalRequests?$filter=Status eq 'W'"
+    url =  settings.sld_url + "/ApprovalRequests?$filter=Status eq 'W'"
     cookies = {"B1SESSION": login_request.json().get('SessionId')}
-    header = {"Prefer": "odata.maxpagesize=2"}
+    header = {"Prefer": "odata.maxpagesize=0"}
     get_approval_documents = requests.get(url, verify=False, cookies=cookies, headers=header)
-    for document in get_approval_documents.json().get('value'):
-        keyboard = types.InlineKeyboardMarkup()  # наша клавиатура
-        document_key = types.InlineKeyboardButton(text=f"Утвердить", callback_data=json.dumps(
-            {"Code": document.get("Code"),
-             "Action": "Approve"}))
-        keyboard.add(document_key)  # добавляем кнопку в клавиатуру
-        question=f"Документ {document.get('Code')} от {document.get('CreationDate')} {document.get('CreationTime')}"
+    documents = get_approval_documents.json().get('value')
+    if len(documents) > 0:
+        for document in documents:
+            print(document)
+            keyboard = types.InlineKeyboardMarkup()  # наша клавиатура
+            document_key_approve = types.InlineKeyboardButton(text=f"Утвердить", callback_data=json.dumps(
+                {"Code": document.get("Code"),
+                 "Action": "Approve"}))
+            document_key_deny = types.InlineKeyboardButton(text=f"Отклонить", callback_data=json.dumps(
+                {"Code": document.get("Code"),
+                 "Action": "Deny"}))
+            keyboard.add(document_key_approve)  # добавляем кнопку в клавиатуру
+            keyboard.add(document_key_deny)  # добавляем кнопку в клавиатуру
+            question=f"""Документ {document.get('Code')} от {document.get('CreationDate')} {document.get('CreationTime')}\n{document.get('Remarks')}\n"""
 
-        bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
-        print(document)
+            if len(document.get("ApprovalRequestLines")) > 0:
+                question += "Этапы утверждения:\n"
+                for line in document.get("ApprovalRequestLines"):
+                    question += f"{line.get('StageCode')} {line.get('Status')}\n"
+
+            bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
+    else:
+        bot.send_message(message.from_user.id, text='Нет документов на утверждение')
 
 
 def accept_approval_request(message, document_id=None):
     login_request = login_to_sld()
     cookies = {"B1SESSION": login_request.json().get('SessionId')}
-    header = {"Prefer": "odata.maxpagesize=2"}
-    url = f'https://192.168.221.171:50000/b1s/v1/ApprovalRequests({document_id})'
+    header = {"Prefer": "odata.maxpagesize=20"}
+    url = settings.sld_url + "/ApprovalRequests({document_id})"
     request_body = {
     "ApprovalRequestDecisions": [
         {
@@ -77,31 +86,52 @@ def accept_approval_request(message, document_id=None):
 
     if approve_request.status_code == 200:
         # bot.send_message(message.from_user.id, text="Ok")
-        pass
+        return True
     else:
         # bot.send_message(message.from_user.id, text="Error")
-        pass
+        return False
 
 
 
-def deny_approval_request(message):
-    pass
+def deny_approval_request(message, document_id=None):
+    login_request = login_to_sld()
+    cookies = {"B1SESSION": login_request.json().get('SessionId')}
+    header = {"Prefer": "odata.maxpagesize=2"}
+    url = settings.sld_url + "/ApprovalRequests({document_id})"
+    request_body = {
+        "ApprovalRequestDecisions": [
+            {
+                "Status": "ardNotApproved",
+                "Remarks": "Approved - Rejected"
+            }
+        ]
+    }
+    approve_request = requests.patch(url, verify=False, cookies=cookies, headers=header, json=request_body)
+
+    if approve_request.status_code == 200:
+        # bot.send_message(message.from_user.id, text="Ok")
+        return True
+    else:
+        # bot.send_message(message.from_user.id, text="Error")
+        return False
 
 
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
-    print(f"{call.data=}")
-    if call.data == "yes": #call.data это callback_data, которую мы указали при объявлении кнопки
-        pass #код сохранения данных, или их обработки
-        bot.send_message(call.message.chat.id, 'Запомню : )')
-    elif call.data == "no":
-         pass #переспрашиваем
-    elif call.data == 'Все документы, требующие утверждения':
+    if call.data == 'Все документы, требующие утверждения':
         pass
     elif json.loads(call.data).get('Code') and json.loads(call.data).get("Action") == "Approve":
         print("callback", call.data)
         accept_approval_request(call.message, json.loads(call.data).get('Code'))
+        bot.send_message(call.message.chat.id, 'Готово')
+        # get_all_approval_requests(call.message)
+    elif json.loads(call.data).get('Code') and json.loads(call.data).get("Action") == "Deny":
+        print("callback", call.data)
+        deny_approval_request(call.message, json.loads(call.data).get('Code'))
+        bot.send_message(call.message.chat.id, 'Готово')
+        # get_all_approval_requests(call.message)
+
 
 bot.polling(none_stop=True, interval=0)
---
+++ settings.py
@@ -0,0 +1,4 @@
+bot_token = ""
+channel = ''
+
+sld_url = 'https://192.168.221.171:50000/b1s/v1'
